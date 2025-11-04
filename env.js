44746d8ecf35f2a34375db23bf17cffa166de9bb
Manuel Fondovila <manuel.fondovila@istea.com.ar>
2025-11-04 19:25:53 -0300

    env.js

---
+++ env.js
@@ -0,0 +1,3 @@
+export const AIRTABLE_TOKEN = "patP6vJJPzM1RCubl.3bc2423ef373b38205025ecb936ffeba5ac6097dd643edcc702697ece4a26eda";
+export const BASE_ID = "appv2Xfi6hr9X1COm";
+export const TABLE_NAME = "Products";
