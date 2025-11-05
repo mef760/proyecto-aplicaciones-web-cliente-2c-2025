193f9fc03ded26689947c287067d46a2c178f499
Manuel Fondovila <manuel.fondovila@istea.com.ar>
2025-11-05 09:54:45 -0300

    Add cart functionality with toast notifications and update cart count display

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import { AIRTABLE_TOKEN, BASE_ID, TABLE_NAME } from './env.js';
+import { ICON_CHECK, ICON_CART } from './icons.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   // dom elements
   const productsDomElements = document.querySelector('.product-container');
   const inputSearch = document.getElementById('input-search-products');
   const categoryLinks = document.querySelectorAll('.category-product-filter');
+  const cartIcon = document.getElementById('cart-icon');
+
+  cartIcon.innerHTML = `${ICON_CART}<span id="cart-count">0</span>`;
+
+  function updateCartCount() {
+    const cartItemsCount = JSON.parse(localStorage.getItem('cart'))?.length || 0;
+
+    if (cartItemsCount > 0) {
+      const cartCountSpan = document.getElementById('cart-count');
+      cartCountSpan.innerText = cartItemsCount;
+    } 
+  }
 
   // airtable config
   const airtableToken = AIRTABLE_TOKEN;
@@ -62,6 +75,15 @@ document.addEventListener('DOMContentLoaded', () => {
       const cart = JSON.parse(localStorage.getItem('cart')) || [];
       cart.push(product);
       localStorage.setItem('cart', JSON.stringify(cart));
+      updateCartCount();
+      const toast = document.getElementById('toast-carrito');
+      toast.style.display = 'flex';
+      toast.innerHTML = `
+        ${ICON_CHECK}
+        <div>${product.name} agregado al carrito</div>`;
+      setTimeout(() => {
+        toast.style.display = 'none';
+      }, 3000);
     });
 
     newDiv.append(newImg, newPName, newPPrice, buttonAddToCart);
@@ -109,4 +131,5 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   getProductsFromAirtable();
+  updateCartCount();
 });
---
+++ icons.js
@@ -0,0 +1,35 @@
+export const ICON_CHECK = `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentcolor"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            >
+            <path d="M9 11l3 3l8 -8" />
+            <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
+        </svg>`;
+
+export const ICON_CART = `
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentcolor"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        >
+        <path d="M8 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+        <path d="M18 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+        <path d="M2 5h2.5l1.632 4.897a6 6 0 0 0 5.693 4.103h2.675a5.5 5.5 0 0 0 0 -11h-.5v6" />
+        <path d="M6 9h14" />
+        <path d="M9 17l1 -3" />
+        <path d="M16 14l1 3" />
+        </svg>
+`;
